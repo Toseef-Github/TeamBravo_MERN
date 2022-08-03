e1f622fa09290baa5519dc63673fa8a92d7bba0b
D-Pats <devinpatel97@gmail.com>
2022-08-03 10:10:18 +0100

    npm install express may have changed the package.json and package-lock.json

--- QA-Cinema/qa-cinema/src/index.js
+++ QA-Cinema/qa-cinema/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+'use strict';
+var path = require('path');
+var express = require('express');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+var app = express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+var staticPath = path.join(__dirname, '/');
+app.use(express.static(staticPath));
+
+// Allows you to set port in the project properties.
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('listening');
+});
