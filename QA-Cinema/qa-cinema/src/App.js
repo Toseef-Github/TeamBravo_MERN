689e30f565192626b6a962fafdb533e677efb41a
D-Pats <devinpatel97@gmail.com>
2022-08-04 11:49:37 +0100

    Added pages to app.js

--- QA-Cinema/qa-cinema/src/App.js
+++ QA-Cinema/qa-cinema/src/App.js
@@ -2,14 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Components/Webpages/Home/Home'
-import OpeningTimes from './Components/Webpages/OpeningTimes/OpeningTimes'
-import DiscussionBoard from './Components/Webpages/DiscussionBoard/DiscussionBoard';
-import ContactUs from './Components/Webpages/ContactUs/ContactUs';
+
+import About from './Components/Webpages/About/About'
 import Booking from './Components/Webpages/Booking/Booking';
+import Classifications from './Components/Webpages/Classifications/Classifications';
+import ContactUs from './Components/Webpages/ContactUs/ContactUs';
+import DiscussionBoard from './Components/Webpages/DiscussionBoard/DiscussionBoard';
+import Home from './Components/Webpages/Home/Home';
 import ListingsGallery from './Components/Webpages/ListingsGallery/ListingsGallery';
 import Location from './Components/Webpages/Location/Location';
 import NewReleases from './Components/Webpages/NewReleases/NewReleases';
+import OpeningTimes from './Components/Webpages/OpeningTimes/OpeningTimes';
+import Payment from './Components/Webpages/Payment/Payment';
+import PlacesToGo from './Components/Webpages/PlacesToGo/PlacesToGo';
+import Screens from './Components/Webpages/Screens/Screens';
+import Seating from './Components/Webpages/Seating/Seating';
 
 
 function App() {
