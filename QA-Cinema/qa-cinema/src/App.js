bb42f67f2b336e678d0ee18d6e48c1c432ac0b71
D-Pats <devinpatel97@gmail.com>
2022-08-04 11:46:02 +0100

    adding routing

--- QA-Cinema/qa-cinema/src/App.js
+++ QA-Cinema/qa-cinema/src/App.js
@@ -4,20 +4,38 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './Components/Webpages/Home/Home'
 import OpeningTimes from './Components/Webpages/OpeningTimes/OpeningTimes'
+import DiscussionBoard from './Components/Webpages/DiscussionBoard/DiscussionBoard';
+import ContactUs from './Components/Webpages/ContactUs/ContactUs';
+import Booking from './Components/Webpages/Booking/Booking';
+import ListingsGallery from './Components/Webpages/ListingsGallery/ListingsGallery';
+import Location from './Components/Webpages/Location/Location';
+import NewReleases from './Components/Webpages/NewReleases/NewReleases';
 
 
 function App() {
   return (
     <Router>
       <div>
-      <Routes>
-        <Route path='/Home' element={<Home />}></Route>
-        <Route path='/OpeningTimes' element={<OpeningTimes />}></Route>
-
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/About' element={<About />}></Route>
+          <Route path='/Booking' element={<Booking />}></Route>
+          <Route path='/Classifications' element={<Classifications />}></Route>
+          <Route path='/ContactUs' element={<ContactUs />}></Route>
+          <Route path='/DiscussionBoard' element={<DiscussionBoard />}></Route>
+          <Route path='/Home' element={<Home />}></Route>
+          <Route path='/ListingsGallery' element={<ListingsGallery />}></Route>
+          <Route path='/Location' element={<Location />}></Route>
+          <Route path='/NewReleases' element={<NewReleases />}></Route>
+          <Route path='/OpeningTimes' element={<OpeningTimes />}></Route>
+          <Route path='/Payment' element={<Payment />}></Route>
+          <Route path='/PlacesToGo' element={<PlacesToGo />}></Route>
+          <Route path='/Screens' element={<Screens />}></Route>
+          <Route path='/Seating' element={<Seating />}></Route>
+        </Routes>
       </div>
     </Router>
-    );
+  );
 }
 
 export default App;
