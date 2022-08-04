1b1c04b54db37da53eb4ce151c320a060fad292a
D-Pats <devinpatel97@gmail.com>
2022-08-04 12:47:08 +0100

    added dev script and creating open times

--- QA-Cinema/qa-cinema/src/Components/Webpages/OpeningTimes/OpeningTimes.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/OpeningTimes/OpeningTimes.js
@@ -2,11 +2,40 @@ import './OpeningTimes.css';
 
 function OpeningTimes() {
     return (
-        <>
-        <h1>Opening Times</h1>
-        <div><h2>Monday</h2></div>
-        <h2>Monday</h2>
-        </>
+        <div id="OpenTimesDiv">
+            <h1 id="OpenTimesh1">Opening Times</h1>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Monday</h2>
+                <p id="OpenTimesp">09:00 - 00:00</p>
+            </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Tuesday</h2>
+                <p id="OpenTimesp">11:00 - 00:00</p>
+            </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Wednesday</h2>
+                <p id="OpenTimesp">11:00 - 00:00</p>
+            </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Thursday</h2>
+                <p id="OpenTimesp">11:00 - 00:00</p>
+                </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Friday</h2>
+                <p id="OpenTimesp">09:00 - 00:00</p>
+                </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Saturday</h2>
+                <p id="OpenTimesp">09:00 - 01:00</p>
+            </div>
+            <div id="OpenTimesDiv2">
+                <h2 id="OpenTimesh2">Sunday</h2>
+                <p id="OpenTimesp">09:00 - 01:00</p>
+            </div>
+            <div id="OpenTimesDiv3">
+                <p>Times are subject to change on Bank Holidays.</p>
+            </div>
+        </div>
     );
 }
 
