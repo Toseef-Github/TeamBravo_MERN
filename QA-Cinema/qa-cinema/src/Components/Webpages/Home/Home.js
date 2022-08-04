f6d3a35e4399ec6141ec18d966c5230c217ae5bd
HodanB <Hodanbarkhad@outlook.com>
2022-08-04 15:23:32 +0100

    Update Home.js

--- QA-Cinema/qa-cinema/src/Components/Webpages/Home/Home.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/Home/Home.js
@@ -1,14 +1,16 @@
+import React from "react";
 import './Home.css';
 
 function Home() {
     return (
         <>
+        
+        <body>
             <div>
                 <h1 id="welcomeTitle">Welcome to QA Cinema</h1>
             </div>
-            <div>
-                
-            </div>
+        </body>
+        
         </>
 
     );
