b951ab9581c9d33442f57dac454bbe562ffd5d22
D-Pats <devinpatel97@gmail.com>
2022-08-04 11:38:17 +0100

    Starting routing for web pages in app.js

--- QA-Cinema/qa-cinema/src/App.js
+++ QA-Cinema/qa-cinema/src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Components/Webpages/Home/Home'
+import OpeningTimes from './Components/Webpages/OpeningTimes/OpeningTimes'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+      <Routes>
+        <Route path='/Home' element={<Home />}></Route>
+        <Route path='/OpeningTimes' element={<OpeningTimes />}></Route>
+
+      </Routes>
+      </div>
+    </Router>
+    );
 }
 
 export default App;
