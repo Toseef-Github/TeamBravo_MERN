7fa7ccf62ae5f69ed1571e109f654b44cf3e919c
HodanB <Hodanbarkhad@outlook.com>
2022-08-05 17:31:43 +0100

    centered carousel

--- QA-Cinema/qa-cinema/src/Components/Webpages/Home/Home.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/Home/Home.js
@@ -11,50 +11,54 @@ function Home() {
                 <h1 id="welcomeTitle">Welcome to QA Cinema</h1>
             </div>
 
-        <Carousel variant="dark" div className="col-lg-10 col-md-12 mb-4 mb-lg-0 border shadow gt-2 text-center card w-80 bg-dark">
+        <Carousel variant="dark" class = "mx-auto" div className="col-lg-8 col-md-12  align-center text-center  card w-8 bg-dark">
         
          <Carousel.Item>
          <img
           className="d-block w-100"
-          src="https://64.media.tumblr.com/2f658e9161e08847d5806017d50da7c8/54efcde873939e47-bb/s540x810/e5fa52740daa0d193d09c929b7d2fef09fcd863e.gifv?text=First slide&bg=f5f5f5"
+          src="https://media3.giphy.com/media/IAo3cBSgXHHhb50cVC/giphy.gif?cid=ecf05e47oef04x2xbqiu7wsly8oga944ufnhniiqrcbw1ilv&rid=giphy.gif&ct=g?text=First slide&bg=f5f5f5"
           alt="First slide"
         />
         <Carousel.Caption>
          <a href="https://github.com"> Minion gru </a>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.
+            
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://thumbs.gfycat.com/SkeletalAssuredBedbug-size_restricted.gif?text=Second slide&bg=eee"
+          src="https://media4.giphy.com/media/sf3PYOIUwF2tiPaSSI/giphy.gif?cid=ecf05e476i5ttk9qixs4omr7557taviqn30stjcigmghq9nj&rid=giphy.gif&ct=g?text=Second slide&bg=eee"
           alt="Second slide"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
+          <h5 id="h5">Second slide label</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item>
-        <img
+        <img 
           className="d-block w-100"
-          src="https://media1.giphy.com/media/pL6mkjytFkTlekNMtM/giphy.gif?cid=ecf05e47c1nj5qlax4tbpq6ewfw5ht15ifgj40qaupbsuilm&rid=giphy.gif&ct=g?text=third slide&bg=e5e5e5"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=e5e5e5"
-          alt="Third slide"
+          src="https://media1.giphy.com/media/pL6mkjytFkTlekNMtM/giphy.gif?cid=ecf05e47c1nj5qlax4tbpq6ewfw5ht15ifgj40qaupbsuilm&rid=giphy.gif&ct=g?text=third slide&bg=e6e5e5"
+          alt="third slide"
         />
         <Carousel.Caption>
           <h5>Third slide label</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://media0.giphy.com/media/nWfnOfr7k6QZLv5Z65/giphy.gif?cid=ecf05e4701mc0gbxzbt4wygqyaiph5r0ufrd8m53z4xctyh7&rid=giphy.gif&ct=g?text=fourth slide&bg=e5e5e5"
+          alt="fourth slide"
+        />
+        <Carousel.Caption>
+          <h5>Fourth slide label</h5>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
           </p>
@@ -71,49 +75,3 @@ function Home() {
 
 export default Home;
 
-// const data = [
-//     {
-//      image: require('./Images/Minion.jpg'), 
-//      caption:"Caption",
-//      description:"Description Here"
-//     },
-//     {
-//       image:require('./Images/bullet-train-movie.jpg'), 
-//       caption:"Caption",
-//       description:"Description Here"
-//      },
-//      {
-//       image:require('./Images/image3.jpg'), 
-//       caption:"Caption",
-//       description:"Description Here"
-//      } 
-//   ]
-  
-//   function HomeCarousel() {
-//     const [index, setIndex] = useState(0);
-//     const handleSelect = (selectedIndex, e) => {
-//       setIndex(selectedIndex);
-//     };
-  
-//     return (
-//       <Carousel activeIndex={index} onSelect={handleSelect}>
-//          {data.map((slide, i) => {
-//           return (
-//             <Carousel.Item>        
-//           <img
-//             className="d-block w-100"
-//             src={slide.image}
-//             alt="slider image"
-//           />
-//           <Carousel.Caption>
-//             <h3>{slide.caption}</h3>
-//             <p>{slide.description}</p>
-//           </Carousel.Caption>
-//         </Carousel.Item>
-//           )
-//         })}
-        
-//       </Carousel>
-//     );
-//   }
-//   export default HomeCarousel;
