bcd31b274cfef628a32679c1d871f4ea32aaaf60
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-08-05 16:45:49 +0100

    updated about

--- QA-Cinema/qa-cinema/src/Components/Webpages/About/About.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/About/About.js
@@ -5,19 +5,17 @@ import Card from 'react-bootstrap/Card';
 
 function About() {
   return (
-<div className="row  text-black">
-    <p id="Listings" />
-    {/* Ladislav Mudry*/}
+<div className="row  text-white">
     <div>
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Devin</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">GitHub</Button>
       </Card.Body>
     </Card>
     </div>
@@ -26,12 +24,40 @@ function About() {
   <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Hodan</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the
       bulk of the card's content.
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Button variant="primary">GitHub</Button>
+  </Card.Body>
+    </Card>
+    </div>
+
+    <div>
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Toseef</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the
+      bulk of the card's content.
+    </Card.Text>
+    <Button variant="primary">GitHub</Button>
+  </Card.Body>
+    </Card>
+    </div>
+
+    <div>
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Waseem</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the
+      bulk of the card's content.
+    </Card.Text>
+    <Button variant="primary">GitHub</Button>
   </Card.Body>
     </Card>
     </div>
