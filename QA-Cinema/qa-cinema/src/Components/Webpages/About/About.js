2a7effb3bb1e002f04344f67f356815775e56951
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-08-05 16:05:41 +0100

    created team member info cards

--- QA-Cinema/qa-cinema/src/Components/Webpages/About/About.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/About/About.js
@@ -1,11 +1,42 @@
 import './About.css';
 
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 function About() {
-    return (
-        <p>
-            About!
-        </p>
-    );
+  return (
+<div className="row  text-black">
+    <p id="Listings" />
+    {/* Ladislav Mudry*/}
+    <div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+
+    <div>
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the
+      bulk of the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+    </Card>
+    </div>
+</div>
+  );
 }
 
 export default About;
