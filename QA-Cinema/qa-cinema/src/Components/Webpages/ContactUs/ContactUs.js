5939d2b777ae1762d9208e0eb915d27c92b1aed8
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-08-05 17:29:13 +0100

    added fields for contact us page

--- QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
@@ -2,9 +2,32 @@ import './ContactUs.css';
 
 function ContactUs() {
     return (
-        <p>
-            Contact us
-        </p>
+<form>
+  <div class="form-group text-white">
+    <label for="Input1">Full Name</label>
+    <input type="text" class="form-control" id="InputName" placeholder="Enter Name Here"></input>
+  </div>
+  <div class="form-group">
+    <label for="InputEmail">Email address</label>
+    <input type="email" class="form-control" id="InputEmail" placeholder="name@example.com"></input>
+  </div>
+  <div class="form-group text-white">
+    <label for="SelectTopic">Topic</label>
+    <select multiple class="form-control" id="SelectTopic">
+      <option>Showings</option>
+      <option>Website</option>
+      <option>Location</option>
+      <option>Bookings</option>
+      <option>Other</option>
+    </select>
+  </div>
+  <div class="form-group text-white">
+    <label for="InputInfo">Tell Us More</label>
+    <textarea class="form-control" id="InputInfo" placeholder="Enter Text Here" rows="3"></textarea>
+  </div>
+  <button type="button" class="btn btn-primary">Submit</button>
+</form>
+
     );
 }
 
