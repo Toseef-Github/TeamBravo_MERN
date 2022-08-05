12488b98075cbd41dd4dfae3134e739b64cc91ac
D-Pats <devinpatel97@gmail.com>
2022-08-05 13:29:35 +0100

    Updated OpeningTimes and PlacesToGo

--- QA-Cinema/qa-cinema/src/Components/Webpages/OpeningTimes/OpeningTimes.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/OpeningTimes/OpeningTimes.js
@@ -5,116 +5,80 @@ import Accordion from 'react-bootstrap/Accordion';
 function OpeningTimes() {
     return (
         <>
-            <div>
-                <h1 id="OpenTimesh1">Opening Times</h1>
-                <Table striped bordered hover variant="dark">
-                    <thead>
-                        <tr>
-                            <th>Day</th>
-                            <th>Open</th>
-                            <th>Close</th>
-                            <th>Last Film start</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Monday</td>
-                            <td>09:00</td>
-                            <td>00:00</td>
-                            <td>22:00</td>
-                        </tr>
-                        <tr>
-                            <td>Tuesday</td>
-                            <td>11:00</td>
-                            <td>00:00</td>
-                            <td>23:00</td>
-                        </tr>
-                        <tr>
-                            <td>Wednesday</td>
-                            <td>11:00</td>
-                            <td>00:00</td>
-                            <td>23:00</td>
-                        </tr>
-                        <tr>
-                            <td>Thursday</td>
-                            <td>11:00</td>
-                            <td>00:00</td>
-                            <td>23:00</td>
-                        </tr>
-                        <tr>
-                            <td>Friday</td>
-                            <td>09:00</td>
-                            <td>01:00</td>
-                            <td>23:30</td>
-                        </tr>
-                        <tr>
-                            <td>Saturday</td>
-                            <td>09:00</td>
-                            <td>01:00</td>
-                            <td>00:00</td>
-                        </tr>
-                        <tr>
-                            <td>Sunday</td>
-                            <td>09:00</td>
-                            <td>01:00</td>
-                            <td>00:00</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-            <div>
-                <Accordion variant="dark">
-                    <Accordion.Item eventKey="0">
-                        <Accordion.Header>Times my vary - More Info</Accordion.Header>
-                        <Accordion.Body>
-                            Bank holiday times will mean that the Cinema will open 1 hour later and close 1 hour earlier. This means that the last viewing will also be 1 hour earlier.
-                            <br />
-                            Times are also subject to change if there are staff shortages.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
-            </div>
+            <body>
+                <div>
+                    <h1 id="OpenTimesh1">Opening Times</h1>
+                    <Table striped bordered hover variant="dark">
+                        <thead>
+                            <tr>
+                                <th>Day</th>
+                                <th>Open</th>
+                                <th>Close</th>
+                                <th>Last Film start</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Monday</td>
+                                <td>09:00</td>
+                                <td>00:00</td>
+                                <td>22:00</td>
+                            </tr>
+                            <tr>
+                                <td>Tuesday</td>
+                                <td>11:00</td>
+                                <td>00:00</td>
+                                <td>23:00</td>
+                            </tr>
+                            <tr>
+                                <td>Wednesday</td>
+                                <td>11:00</td>
+                                <td>00:00</td>
+                                <td>23:00</td>
+                            </tr>
+                            <tr>
+                                <td>Thursday</td>
+                                <td>11:00</td>
+                                <td>00:00</td>
+                                <td>23:00</td>
+                            </tr>
+                            <tr>
+                                <td>Friday</td>
+                                <td>09:00</td>
+                                <td>01:00</td>
+                                <td>23:30</td>
+                            </tr>
+                            <tr>
+                                <td>Saturday</td>
+                                <td>09:00</td>
+                                <td>01:00</td>
+                                <td>00:00</td>
+                            </tr>
+                            <tr>
+                                <td>Sunday</td>
+                                <td>09:00</td>
+                                <td>01:00</td>
+                                <td>00:00</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+                <div>
+                    <Accordion id="OpeningTimesWarningAccord">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header>Times my vary - More Info</Accordion.Header>
+                            <Accordion.Body>
+                                Bank holiday times will mean that the Cinema will open 1 hour later and close 1 hour earlier. This means that the last viewing will also be 1 hour earlier.
+                                <br />
+                                Times are also subject to change if there are staff shortages.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
+                </div>
+            </body>
         </>
+
     );
 }
-
-// function OpeningTimes() {
-//     return (
-//         <div id="OpenTimesDiv">
-//             <h1 id="OpenTimesh1">Opening Times</h1>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Monday</h2>
-//                 <p id="OpenTimesp">09:00 - 00:00</p>
-//             </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Tuesday</h2>
-//                 <p id="OpenTimesp">11:00 - 00:00</p>
-//             </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Wednesday</h2>
-//                 <p id="OpenTimesp">11:00 - 00:00</p>
-//             </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Thursday</h2>
-//                 <p id="OpenTimesp">11:00 - 00:00</p>
-//                 </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Friday</h2>
-//                 <p id="OpenTimesp">09:00 - 00:00</p>
-//                 </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Saturday</h2>
-//                 <p id="OpenTimesp">09:00 - 01:00</p>
-//             </div>
-//             <div id="OpenTimesDiv2">
-//                 <h2 id="OpenTimesh2">Sunday</h2>
-//                 <p id="OpenTimesp">09:00 - 01:00</p>
-//             </div>
-//             <div id="OpenTimesDiv3">
-//                 <p>Times are subject to change on Bank Holidays.</p>
-//             </div>
-//         </div>
-//     );
-// }
 
 export default OpeningTimes;
--- QA-Cinema/qa-cinema/src/Components/Webpages/PlacesToGo/PlacesToGo.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/PlacesToGo/PlacesToGo.js
@@ -1,10 +1,106 @@
 import './PlacesToGo.css';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import CardGroup from 'react-bootstrap/CardGroup';
 
 function PlacesToGo() {
     return (
-        <p>
-            PlacesToGo!
-        </p>
+        <>
+            <h1 id="PlacesToGoh1">Places To Go</h1>
+            <div>
+                <h2 id="PlacesToGoh2">Restaurants</h2>
+                {/* <CardGroup> */}
+                    <Card bg='dark' key='dark' border='dark' text='white' style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                        <Card.Body>
+                            <Card.Title>Tesco</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Rating: </ListGroup.Item>
+                            <ListGroup.Item>Price: </ListGroup.Item>
+                            <ListGroup.Item>Walking time: </ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                {/* </CardGroup> */}
+            </div>
+            <div>
+                <h2 id="PlacesToGoh2">Bars</h2>
+                {/* <CardGroup> */}
+                    <Card bg='dark' key='dark' border='dark' text='white' style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                        <Card.Body>
+                            <Card.Title>Slug & Lettuce</Card.Title>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroup.Item>Rating: </ListGroup.Item>
+                            <ListGroup.Item>Price: </ListGroup.Item>
+                            <ListGroup.Item>Walking time: </ListGroup.Item>
+                        </ListGroup>
+                        <Card.Body>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                {/* </CardGroup> */}
+            </div>
+            <div>
+                <h2 id="PlacesToGoh2">Experiences</h2>
+                <div>
+                    <CardGroup>
+                        <Card bg='dark' key='dark' border='dark' text='white' style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                            <Card.Body>
+                                <Card.Title>Tower of London</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroup.Item>Rating: </ListGroup.Item>
+                                <ListGroup.Item>Price: </ListGroup.Item>
+                                <ListGroup.Item>Walking time: </ListGroup.Item>
+                            </ListGroup>
+                            <Card.Body>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                        <Card bg='dark' key='dark' border='dark' text='white' style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+                            <Card.Body>
+                                <Card.Title>Tower Bridge</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroup.Item>Rating: </ListGroup.Item>
+                                <ListGroup.Item>Price: </ListGroup.Item>
+                                <ListGroup.Item>Walking time: </ListGroup.Item>
+                            </ListGroup>
+                            <Card.Body>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </div>
+            </div>
+        </>
     );
 }
 
