5c89f6ccf2c436f4f263ff3f5c599be336bf130c
D-Pats <devinpatel97@gmail.com>
2022-08-08 10:53:38 +0100

    Updated classifications 18 content tab

--- QA-Cinema/qa-cinema/src/Components/Webpages/Classifications/Classifications.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/Classifications/Classifications.js
@@ -265,9 +265,11 @@ function Classifications() {
                                             <li>
                                                 where the material is in breach of the criminal law, or has been created through the commission of a criminal offence.
                                             </li>
+                                            <br />
                                             <li>
                                                 where material or treatment appears to us to risk harm to individuals or, through their behaviour, to society. For example, the detailed portrayal of violent or dangerous acts, or of illegal drug use, which may cause harm to public health or morals. This may include portrayals of sadistic violence, rape or other non-consensual sexually violent behaviour which make this violence look appealing; reinforce the suggestion that victims enjoy rape or other non-consensual sexually violent behaviour; or which invite viewer complicity in rape, other non-consensual sexually violent behaviour or other harmful violent activities.
                                             </li>
+                                            <br />
                                             <li>
                                                 where there are more explicit images of sexual activity in the context of a sex work (see right) In the case of video works, which may be more accessible to younger viewers, intervention may be more frequent than for cinema films.
                                             </li>
