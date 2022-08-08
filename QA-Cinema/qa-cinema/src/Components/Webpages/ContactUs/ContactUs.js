6ba8fecd6565a6281718bc63da90b5261481a876
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-08-08 15:24:28 +0100

    added backdrop and aligned text

--- QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
@@ -2,7 +2,7 @@ import './ContactUs.css';
 
 function ContactUs() {
     return (
-<form>
+<form action="https://formsubmit.co/toseef.mehdi@hotmail.co.uk" method="POST">
   <div class="form-group text-white">
     <label for="Input1">Full Name</label>
     <input type="text" class="form-control" id="InputName" placeholder="Enter Name Here"></input>
@@ -25,7 +25,7 @@ function ContactUs() {
     <label for="InputInfo">Tell Us More</label>
     <textarea class="form-control" id="InputInfo" placeholder="Enter Text Here" rows="3"></textarea>
   </div>
-  <button type="button" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
     );
