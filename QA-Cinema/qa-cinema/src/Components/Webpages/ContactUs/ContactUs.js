08b9cd8c0dd79deb5f35bf68bd5968a61c649704
Toseef-Github <105277464+Toseef-Github@users.noreply.github.com>
2022-08-11 14:46:51 +0100

    updated contact us page with working submit button and css

--- QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
+++ QA-Cinema/qa-cinema/src/Components/Webpages/ContactUs/ContactUs.js
@@ -2,31 +2,33 @@ import './ContactUs.css';
 
 function ContactUs() {
     return (
-<div id="form-div">
-  <form class="form" id="form1" action="https://formsubmit.co/toseef.mehdi@hotmail.co.uk" method="POST">
-  <div class="form-group text-white">
-    <label for="Input1">Full Name</label>
-    <input type="text" class="form-control" id="InputName" placeholder="Enter Name Here"></input>
+<div class="text-white" id="form-div">
+  <form align="center" class="form" id="form1" action="https://formsubmit.co/toseef.mehdi@hotmail.co.uk" method="POST">
+  <div><label for="Input1">Full Name:</label>
   </div>
-  <div class="form-group text-white">
-    <label for="InputEmail">Email address</label>
-    <input type="email" class="form-control" id="InputEmail" placeholder="name@example.com"></input>
+  <input type="text" name="name" required></input>
+<div>
+  <label for="InputEmail">Email address:</label>
   </div>
-  <div class="form-group text-white">
-    <label for="SelectTopic">Topic</label>
-    <select multiple class="form-control" id="SelectTopic">
+  <input type="text" name="email" required></input>
+  <div>
+  <label for="SelectTopic">Topic</label>
+  </div>
+    <select>
       <option>Showings</option>
       <option>Website</option>
       <option>Location</option>
       <option>Bookings</option>
       <option>Other</option>
     </select>
+
+<div>
+  <label for="InputInfo">Tell Us More:</label>
   </div>
-  <div class="form-group text-white">
-    <label for="InputInfo">Tell Us More</label>
-    <textarea class="form-control" id="InputInfo" placeholder="Enter Text Here" rows="3"></textarea>
-  </div>
+  <input type="text" name="moreinfo" required></input>
+<div>
   <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
 </form>
 </div>
     );
