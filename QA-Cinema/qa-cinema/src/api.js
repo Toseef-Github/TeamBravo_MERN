b7433cbae5e77c4285ea3020060c469327515122
D-Pats <devinpatel97@gmail.com>
2022-08-11 14:42:53 +0100

    Changed user and background colour

--- QA-Cinema/qa-cinema/src/api.js
+++ QA-Cinema/qa-cinema/src/api.js
@@ -8,7 +8,7 @@ export const createComment = async (text, parentId = null) => {
     body: text,
     parentId,
     userId: "1",
-    username: "Devin",
+    username: "User",
     createdAt: new Date().toISOString(),
   };
 };
